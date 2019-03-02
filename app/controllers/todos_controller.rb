07f8b8f4ef8969d4a27594b9ecb135b5763f96dd
ektigah <ektigah@gmail.com>
2019-03-02 04:09:05 -0500

    part two complete

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,22 +1,23 @@
-# app/controllers/todos_controller.rb
 class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :update, :destroy]
 
   # GET /todos
   def index
-    @todos = Todo.all
+    # get current user todos
+    @todos = current_user.todos
     json_response(@todos)
-  end
-
-  # POST /todos
-  def create
-    @todo = Todo.create!(todo_params)
-    json_response(@todo, :created)
   end
 
   # GET /todos/:id
   def show
     json_response(@todo)
+  end
+
+  # POST /todos
+  def create
+    # create todos belonging to current user
+    @todo = current_user.todos.create!(todo_params)
+    json_response(@todo, :created)
   end
 
   # PUT /todos/:id
@@ -33,9 +34,9 @@ class TodosController < ApplicationController
 
   private
 
+  # remove `created_by` from list of permitted parameters
   def todo_params
-    # whitelist params
-    params.permit(:title, :created_by)
+    params.permit(:title)
   end
 
   def set_todo
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
-# config/routes.rb
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :todos do
     resources :items
   end
+
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
 end
